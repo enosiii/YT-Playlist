028fa99d2937494820510f6d7b84f831e70bd0ab
enosiii <121915517+enosiii@users.noreply.github.com>
2025-02-01 22:47:19 +0800

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,19 @@
+
+//playlist.js
+
+const playlists = [
+    { title: "Top Song PH", id: "PLfvBgYzp9y6QNl-TSAo9JNHgTUtJX3KdA", count: " 90"},
+
+    { title: "Anime OST", id: "PL_Wda96yb1UAGlZaDVDTnisjwtuAZSq1D",  count: " 150"},
+
+    { title: "Spotify Playlist PH", id: "PLTVRe9aECZ6e223yE0OLusgwfqxqL6jam", count: " 65"},
+
+    { title: "Chill OPM Music", id: "PLOwSo8kHs4XQh9lmu74lkdYimwOZ1V-0c", count: " 65"},
+
+    { title: "Rock Songs", id: "PLplXQ2cg9B_pqNuu7_tVKgFGhrL7IWIYf", count: " 100"},
+
+    { title: "Ms. Rachel Baby", id: "PLEeIyWKd-HQ5vnV6HLlGojLo-_THu-VSm", count: " 3"},
+
+    { title: "Ms. Rachel Nursery Rhymes", id: "PLEeIyWKd-HQ4pxSdPQyWTD7TsMqbdXZVi", count: " 8"}
+
+];
