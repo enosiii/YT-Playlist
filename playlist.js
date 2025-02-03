62b8609b46576283852dde8fead9022badc6778c
enosiii <121915517+enosiii@users.noreply.github.com>
2025-02-04 07:04:47 +0800

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,26 +1,26 @@
 
-
 //================================
 //playlist.js
 
 const playlists = [
-    { title: "Top Song PH", id: "PLfvBgYzp9y6QNl-TSAo9JNHgTUtJX3KdA", count: " 90"},
+    { title: "Top Song PH", id: "PLfvBgYzp9y6QNl-TSAo9JNHgTUtJX3KdA", count: "90"},
+
+
+    { title: "Anime Songs", id: "RDCLAK5uy_mRcc2Y3l-RoZsDt27qu8CBGpKt-5w7v8g",  count: "200"},
 
     { title: "Anime OST", id: "PL_Wda96yb1UAGlZaDVDTnisjwtuAZSq1D",  count: " 150"},
 
-    { title: "Spotify Playlist PH", id: "PLTVRe9aECZ6e223yE0OLusgwfqxqL6jam", count: " 65"},
+    { title: "Spotify Playlist PH", id: "PLTVRe9aECZ6e223yE0OLusgwfqxqL6jam", count: "65"},
 
-    { title: "Chill OPM Music", id: "PLOwSo8kHs4XQh9lmu74lkdYimwOZ1V-0c", count: " 65"},
+    { title: "Chill OPM Music", id: "PLOwSo8kHs4XQh9lmu74lkdYimwOZ1V-0c", count: "65"},
 
-    { title: "Rock Songs", id: "PLplXQ2cg9B_pqNuu7_tVKgFGhrL7IWIYf", count: " 100"},
+    { title: "Rock Songs", id: "PLplXQ2cg9B_pqNuu7_tVKgFGhrL7IWIYf", count: "100"},
 
-    { title: "MAKI Playlist", id: "PLiy0XOfUv4hFQUEIeE53y4M7vErN0oq8c", count: " 3"},
+    { title: "MAKI Playlist", id: "PLiy0XOfUv4hFQUEIeE53y4M7vErN0oq8c", count: "3"},
 
-    { title: "Ms. Rachel Baby", id: "PLEeIyWKd-HQ5vnV6HLlGojLo-_THu-VSm", count: " 3"},
+    { title: "Ms. Rachel Baby", id: "PLEeIyWKd-HQ5vnV6HLlGojLo-_THu-VSm", count: "3"},
 
-    { title: "Ms. Rachel Nursery Rhymes", id: "PLEeIyWKd-HQ4pxSdPQyWTD7TsMqbdXZVi", count: " 8"}
+    { title: "Ms. Rachel Nursery Rhymes", id: "PLEeIyWKd-HQ4pxSdPQyWTD7TsMqbdXZVi", count: "8"}
 
 ];
-
-
 
