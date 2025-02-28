cd299e9c3b358c49fb187ccf3f998dae65750848
enosiii <121915517+enosiii@users.noreply.github.com>
2025-02-28 16:12:11 +0800

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -5,7 +5,6 @@
 const playlists = [
     { title: "Top Song PH", id: "PLfvBgYzp9y6QNl-TSAo9JNHgTUtJX3KdA", count: "90"},
 
-
     { title: "Anime Songs", id: "RDCLAK5uy_mRcc2Y3l-RoZsDt27qu8CBGpKt-5w7v8g",  count: "200"},
 
     { title: "Anime OST", id: "PL_Wda96yb1UAGlZaDVDTnisjwtuAZSq1D",  count: " 150"},
@@ -17,6 +16,8 @@ const playlists = [
     { title: "Rock Songs", id: "PLplXQ2cg9B_pqNuu7_tVKgFGhrL7IWIYf", count: "100"},
 
     { title: "MAKI Playlist", id: "PLiy0XOfUv4hFQUEIeE53y4M7vErN0oq8c", count: "100"},
+
+    { title: "99 Red Balloons", id: "PLTgVPQThJ32xtsHJ71rx5LUtrNlZlb5l_", count: " 27"},
 
     { title: "Ms. Rachel Baby", id: "PLEeIyWKd-HQ5vnV6HLlGojLo-_THu-VSm", count: "3"},
 
